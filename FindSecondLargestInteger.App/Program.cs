3fd6d8e0bd73ea491a5d3a7235d62a99f6558e7c
Christopher Plummer <cplummer@reservations.com>
2021-09-22 14:17:13 -0400

    Initial commit

---
+++ FindSecondLargestInteger.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FindSecondLargestInteger.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void CheckForSecondLargest(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
