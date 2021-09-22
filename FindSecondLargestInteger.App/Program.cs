278763596a117b9c61d465793336ea843ded5629
Christopher Plummer <cplummer@reservations.com>
2021-09-22 14:50:08 -0400

    Coded the solution

--- FindSecondLargestInteger.App/Program.cs
+++ FindSecondLargestInteger.App/Program.cs
@@ -6,15 +6,68 @@ namespace FindSecondLargestInteger.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 9, 44, 66, 3, 210 };
+            
+            CheckForSecondLargest(numbers);
         }
 
-        public static void CheckForSecondLargest(int[] numbers)
+        private static void CheckForSecondLargest(int[] numbers)
         {
-            for (int i = 0; i < numbers.Length; i++)
+            // Check if array is null or empty
+            if (IsNullOrEmpty(numbers))
             {
-                
+                Console.WriteLine("Array is null or empty");
+                return;
             }
+
+            // First number will be our first max
+            int maxNumber = numbers[0];
+            int secondToMaxNumber;
+
+            // If we have more than 1 number, the second position in the array will be first secondToMax
+            if (numbers.Length > 1)
+            {
+                secondToMaxNumber = numbers[1];
+
+                // If we only have 2 numbers, calculate the secondToMax
+                if (numbers.Length < 3)
+                {
+                    secondToMaxNumber = maxNumber > secondToMaxNumber ? secondToMaxNumber : maxNumber;
+                    PrintResult(secondToMaxNumber);
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Only 1 number exists.");
+                return;
+            }
+            
+            // Iterate array to find true secondToMax
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxNumber)
+                {
+                    secondToMaxNumber = maxNumber;
+                    maxNumber = numbers[i];
+                }
+                else if (numbers[i] > secondToMaxNumber)
+                {
+                    secondToMaxNumber = numbers[i];
+                }
+            }
+            
+            PrintResult(secondToMaxNumber);
+        }
+
+        private static bool IsNullOrEmpty(int[] numbers)
+        {
+            return numbers is not { Length: > 0 };
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.WriteLine($"The second to largest integer is {result}");
         }
     }
 }
